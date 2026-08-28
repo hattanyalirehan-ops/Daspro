fe8b3274408c972c4ee3e6b7821527494950b1a3
hattanyalirehan-ops <hattanyalirehan@gmail.com>
2026-08-29 02:18:34 +0700

    menambahkan file java

---
+++ MyFirstJava26.java
@@ -0,0 +1,8 @@
+public class MyFirstJava26 {
+   public static void main (String[] args) {
+    System.out.print("Hello World");
+    if (exp instanceof Object) {
+        Object obj = (Object)exp;
+    }
+  }
+}
