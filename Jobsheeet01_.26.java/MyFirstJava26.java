22c46c2f3c52c1d40e5fcae5a75463fb43ceac85
hattanyalirehan-ops <hattanyalirehan@gmail.com>
2026-09-02 09:36:01 +0700

    Delete Jobsheeet01_.26.java directory

--- Jobsheeet01_.26.java/MyFirstJava26.java
+++
@@ -1,9 +0,0 @@
-package Project.Minggu1;
-public class MyFirstJava26 {
-   public static void main (String[] args) {
-    System.out.print("Hello World");
-    if (exp instanceof Object) {
-        Object obj = (Object)exp;
-    }
-  }
-}
